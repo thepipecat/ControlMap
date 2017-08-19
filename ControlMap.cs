55022abf300a5e4a03b7c9535ce8fce3acdf0160
Pedro Xudre <pedroxudre@gmail.com>
2017-08-19 16:43:49 -0300

    First setup.

---
+++ ControlMap.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+namespace ThePipeCat
+{
+  public class ControlMap : MonoBehaviour
+  {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+  }
+
+}
